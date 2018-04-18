8452c7abddb7a69b39ef91e885a2e15ce289ca5a
Jeferson Job <jeferson.job@outlook.com>
2018-04-18 18:14:39 -0400

    Inserindo service Pessoa

---
+++ frontend/src/app/services/pessoa.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Headers } from '@angular/http';
+import { RequestOptions } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Rx';
+
+import { Pessoa } from '../services/pessoa';
+import { ConfigService } from '../services/config.service';
+
+@Injectable()
+export class PessoaService
+{
+    private baseUrlService:string = '';
+    private headers:Headers;
+    private options:RequestOptions;
+
+    constructor(private http:Http, private configService: ConfigService)
+    {
+        this.baseUrlService = configService.getUrlService() + '/pessoa/';
+
+        this.headers = new Headers({'Content-Type' : 'application/json;charset=UTF-8'});
+        this.options = new RequestOptions({'headers' : this.headers});
+    }
+
+    getPessoas()
+    {
+        return this.http.get(this.baseUrlService).map(res => res.json());
+    }
+
+    addPessoa(pessoa: Pessoa)
+    {
+        return this.http.post(this.baseUrlService, JSON.stringify(pessoa), this.options)
+        .map(res => res.json());
+    }
+}
